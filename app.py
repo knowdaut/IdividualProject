8a7ecb1c9b0ead2b50360f9b09cc3b479ba1c10d
knowdaut <nickdaut975@gmail.com>
2021-11-03 15:52:52 +0000

    potter

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():                    
     return render_template("index.html") 
 
+@app.route("/potter")                
+def potter():                    
+    return render_template("potter.html") 
+
 if __name__ == "__main__":
     app.run(debug=True) 
